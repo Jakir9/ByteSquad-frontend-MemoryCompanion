274304bdb0d94ffc2215faffc8cce4b60215318a
Goose <victoriaplatt@loveplanes.co.uk>
2023-06-13 12:03:26 +0100

    Time capsule button - Router added

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import NavBar from "../NavBar";
 import FriendsAndFamily from "../FriendsAndFamily";
 import Medication from "../Medication"
 import Events from "../Events"
+import TimeCapsule from "../TimeCapsule"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/friends&family" element={<FriendsAndFamily />}></Route>
           <Route path="/medication" element={<Medication />}></Route>
           <Route path="/events" element={<Events />}></Route>
+          <Route path="/timecapsule" element={<TimeCapsule/>}></Route>
       </Routes>
       <footer>ByteSquad</footer>
       </div>
