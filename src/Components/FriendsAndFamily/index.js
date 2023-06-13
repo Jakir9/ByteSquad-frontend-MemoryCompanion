93a60851e0a71ac71a0e9d620b193b92860bc2b1
Jakir9 <jashraf98@hotmail.co.uk>
2023-06-13 14:29:21 +0100

    hardcoded the fnf cards, text and image

--- src/Components/FriendsAndFamily/index.js
+++ src/Components/FriendsAndFamily/index.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import chris from './chris.jpeg'
+import sarah from './sarah.jpeg'
 
-function index() {
+function FriendsAndFamily() {
   return (
-    <div>
-      <p>   Yello!</p>
-    </div>
+    <>
+      <div className="fnf-1">
+        <figure style={{ width: 250 }}>
+          {' '}
+          <button style={{ display: 'flex', alignitems: 'center' }}> X </button>
+          <img
+            style={{ height: 300, width: 200 }}
+            src={chris}
+            alt="Chris"
+          />{' '}
+        </figure>
+
+        <figcaption> Chris, my Grandson</figcaption>
+      </div>
+
+      <div className="fnf-2">
+        {' '}
+        <figure>
+          {' '}
+          <img
+            style={{ height: 300, width: 200 }}
+            src={sarah}
+            alt="Sarah"
+          />{' '}
+        </figure>
+        <figcaption> Sarah, my Granddaughter</figcaption>
+      </div>
+    </>
   )
 }
 
-export default index
+export default FriendsAndFamily
