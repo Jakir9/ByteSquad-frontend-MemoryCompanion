6eda5123ad8f96de2780f52ab5af6a72386a393f
Darren Broomhall <d.p.broomhall@gmail.com>
2023-06-21 16:07:20 +0100

    login paged full styled

--- src/Components/Login/LoginButton.js
+++ src/Components/Login/LoginButton.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import styles from './styles.css'
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0()
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>
+  return (
+    <div className='login-buttons'>
+  <button onClick={() => loginWithRedirect()}>LOG IN</button>
+  <button onClick={() => loginWithRedirect()}>SIGN UP</button>
+  </div>
+  )
 }
 
 export default LoginButton
--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react'
+import styles from './styles.css'
 import LoginButton from './LoginButton'
-
+import logo from './Memory Companion Logo V2.png'
 //create a login landing page
 
 function Login() {
   return (
-    <>
-      <h1>Login Page Test </h1>
+  <>
+    <div className='container'>
+      <img src={logo} alt='logo' className='logo-login' />
+      <p className='login-text'>Welcome to Memory Companion, the user-friendly app designed to support individuals with memory-related conditions like Dementia.
+      <br></br>
+      <br></br>
+      Easily store your memories, access visual and audio prompts, and receive daily activity reminders. Let us be your trusted companion on this journey of preserving and cherishing memories.</p>
       <LoginButton />
-    </>
+    </div>
+     
+  </>
   )
 }
 export default Login
