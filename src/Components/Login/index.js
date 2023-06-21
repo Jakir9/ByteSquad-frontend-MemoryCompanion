87382b7eeaad40763dafe5d578f6c77574f609e6
Jakir9 <jashraf98@hotmail.co.uk>
2023-06-21 16:51:23 +0100

    added conditional rendering dashboard in login

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import LoginButton from './LoginButton'
 import Dashboard from '../Dashboard/index.js'
+import isLoggedIn from './LoginButton'
 
 //create a login landing page
 
 function Login() {
+  if (isLoggedIn) {
+    return <Dashboard />
+  }
   return (
     <>
       <h1>Login Page Test - login </h1>
       <LoginButton />
-      <Dashboard />
     </>
   )
 }
