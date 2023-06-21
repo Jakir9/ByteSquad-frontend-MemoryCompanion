96470ab5c0870ba7e46ac148e1a105e1f38dca91
Jakir9 <jashraf98@hotmail.co.uk>
2023-06-21 23:16:01 +0100

    Revert "login paged full styled"
    This reverts commit 6eda5123ad8f96de2780f52ab5af6a72386a393f.

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,28 +1,13 @@
-import React from 'react'
-import { useAuth0 } from '@auth0/auth0-react'
-import { useNavigate } from 'react-router-dom'
-// test delete later - pushing again because vercel not deploying latest version
+import React, { Component } from 'react'
+import LoginButton from './LoginButton'
 
-const Login = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0()
-  const navigate = useNavigate()
-
-  const handleLogin = () => {
-    loginWithRedirect()
-  }
-
-  // Check if the user is already authenticated, and if so, redirect to the dashboard
-  React.useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/dashboard')
-    }
-  }, [isAuthenticated, navigate])
-
+//create a login landing page
+function Login() {
   return (
-    <div>
-      <h2>Login Page</h2>
-      <button onClick={handleLogin}>Log In</button>
-    </div>
+    <>
+      <h1>Login Page Test </h1>
+      <LoginButton />
+    </>
   )
 }
 
