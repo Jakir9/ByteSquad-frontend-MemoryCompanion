b74b07f8750d1ead55f0ccd5a5302416c9d3ddac
dannishuttleworth <124736568+dannishuttleworth@users.noreply.github.com>
2023-06-28 11:58:25 +0100

    Successfully console logging the local image url

--- src/Components/Events/index.js
+++ src/Components/Events/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import EventCard from '../Card/EventCard';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router';
-import './styles.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import EventCard from "../Card/EventCard";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router";
+import "./styles.css";
 
-const URL = "http://localhost:3002/api/events/"
+const URL = "http://localhost:3002/api/events/";
 
 function Events() {
   // auth0 code
@@ -14,42 +14,42 @@ function Events() {
   useEffect(() => {
     if (!isAuthenticated) {
       // User is not logged in, redirect to login page
-      navigate('/login');
+      navigate("/login");
     }
   }, [isAuthenticated, navigate]);
   // end auth0 code
   const [eventsList, setEventsList] = useState([]);
   const [addEvent, setAddEvent] = useState(false);
-  const [eventName, setEventName] = useState('');
-  const [dateOfEvent, setDateOfEvent] = useState('');
-  const [eventTime, setEventTime] = useState('');
+  const [eventName, setEventName] = useState("");
+  const [dateOfEvent, setDateOfEvent] = useState("");
+  const [eventTime, setEventTime] = useState("");
 
-//useEffect bellow is used to call the fetch event list function when component is mounted
+  //useEffect bellow is used to call the fetch event list function when component is mounted
 
-useEffect (() => {
-  fetchEventsList();
-}, []);
+  useEffect(() => {
+    fetchEventsList();
+  }, []);
 
-// || GET METHOD || //
+  // || GET METHOD || //
   // The fetchEventsList async function is used to fetch the list of events from the server.
   // This can be called when the component is mounted, or when a new event is added
-async function fetchEventsList() {
-  try {
-    const response = await fetch(URL);
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${response.status}`);
+  async function fetchEventsList() {
+    try {
+      const response = await fetch(URL);
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(`This is the data`, data);
+      setEventsList(Array.isArray(data.payload) ? data.payload : []);
+      console.log(`This is the data.payload`, data.payload);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setEventsList([]);
     }
-    const data = await response.json();
-    console.log(`This is the data`, data);
-    setEventsList(Array.isArray(data.payload) ? data.payload : []);
-    console.log(`This is the data.payload`, data.payload);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    setEventsList([]);
   }
-}
 
-// || POST METHOD || //
+  // || POST METHOD || //
   // The below function is called when a new event is created (form submitted).
   // It creates a new object for the new event, and then sends a POST request to the server to add the new event to the database.
   // Once the event is added, the event list is updated to include the new event.
@@ -63,87 +63,83 @@ async function fetchEventsList() {
       event_date: dateOfEvent,
       event_time: eventTime,
     };
- try{
-  const response= await fetch(URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newEvent),
- });
+    try {
+      const response = await fetch(URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEvent),
+      });
 
- if (response.ok) {
-  // Handle successful response
-  console.log("Event added successfully!");
+      if (response.ok) {
+        // Handle successful response
+        console.log("Event added successfully!");
 
-  // Update the events list in your UI if needed
-  const data = await response.json();
-  setEventsList([...eventsList, data]);
-  fetchEventsList();
-} else {
-  // Handle error
-  throw new Error("Failed to add event");
-}
-} catch (error) {
-// Handle error
-console.error("Error adding event:", error);
-}
+        // Update the events list in your UI if needed
+        const data = await response.json();
+        setEventsList([...eventsList, data]);
+        fetchEventsList();
+      } else {
+        // Handle error
+        throw new Error("Failed to add event");
+      }
+    } catch (error) {
+      // Handle error
+      console.error("Error adding event:", error);
+    }
 
-setEventName("");
-setDateOfEvent("");
-setEventTime("");
-setAddEvent(false);
-}
+    setEventName("");
+    setDateOfEvent("");
+    setEventTime("");
+    setAddEvent(false);
+  }
 
-  
   function handleClick() {
     setAddEvent(true);
   }
   // Function to handle when the delete button is clicked
 
-// || DELETE METHOD || //
+  // || DELETE METHOD || //
   // Function to handle when the delete button is clicked
 
   async function handleDelete(id) {
     // Go through the array and find the person with the matching id
     // Immutably update the array without the person with the matching id
 
-
-    try{ const response = await fetch(`${URL}/${id}`,{
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" ,},
-    });
-    if (response.ok) {
-      fetchEventsList();
-      console.log("Event deleted successfully!");
-    } else {
-      console.log("Failed to delete event");
-    }}
-    catch (error) {
+    try {
+      const response = await fetch(`${URL}/${id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        fetchEventsList();
+        console.log("Event deleted successfully!");
+      } else {
+        console.log("Failed to delete event");
+      }
+    } catch (error) {
       console.error("Error deleting event:", error);
-    }}
+    }
+  }
 
+  // setEventsList(eventsList.filter((item) => item.id !== id));
+  // console.log(eventsList);
 
-
-
-
-    // setEventsList(eventsList.filter((item) => item.id !== id));
-    // console.log(eventsList);
-  
   // Function to format the date in the desired format
   function formatDate(dateString) {
     const eventDate = new Date(dateString);
-    const formattedDate = eventDate.toLocaleDateString('en-US', {
-      month: 'long',
-      day: 'numeric',
-      year: 'numeric',
+    const formattedDate = eventDate.toLocaleDateString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
     });
     return formattedDate;
   }
   return (
     isAuthenticated && (
       <>
-        <h1 className='event-header'>EVENTS</h1>
+        <h1 className="event-header">EVENTS</h1>
         {!addEvent && (
           <>
             {eventsList.map((item) => (
@@ -158,9 +154,9 @@ setAddEvent(false);
             ))}
             {/* This is what will show when we first load the page */}
             <div className="event-button-container">
-            <button className="add-event-button" onClick={handleClick}>
-              Add Event
-            </button>
+              <button className="add-event-button" onClick={handleClick}>
+                Add Event
+              </button>
             </div>
           </>
         )}
@@ -179,7 +175,7 @@ setAddEvent(false);
               </label>
               <br />
               <label>
-              Date of Event:
+                Date of Event:
                 <input
                   className="input-date"
                   type="date"
--- src/Components/Upload/upload.js
+++ src/Components/Upload/upload.js
@@ -1,32 +1,44 @@
-import React, { useState } from "react";
-import axios from "axios";
+// import React, { useState } from "react";
+// import axios from "axios";
 
-function FileUpload() {
-  const [selectedFile, setSelectedFile] = useState(null);
-
-  const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
+function FileUpload({
+  handleFileChange,
+  handleFileUpload,
+  selectedFile,
+  setSelectedFile,
+}) {
+  const handleUpload = () => {
+    handleFileUpload();
+    setSelectedFile(null);
   };
 
-  const handleFileUpload = () => {
-    const formData = new FormData();
-    formData.append("file", selectedFile);
-    // Local Host Port needs to be the backend server port
-    axios.post("http://localhost:3001/upload", formData)
-      .then((response) => {
-        // File upload was successful
-        console.log(response.data);
-      })
-      .catch((error) => {
-        // File upload failed
-        console.error(error);
-      });
-  };
+  // const [selectedFile, setSelectedFile] = useState(null);
+
+  // const handleFileChange = (event) => {
+  //   setSelectedFile(event.target.files[0]);
+  //   console.log(selectedFile);
+  // };
+
+  // const handleFileUpload = () => {
+  //   const formData = new FormData();
+  //   formData.append("file", selectedFile);
+  //   // Local Host Port needs to be the backend server port
+  //   axios
+  //     .post("http://localhost:3002/upload", formData)
+  //     .then((response) => {
+  //       // File upload was successful
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       // File upload failed
+  //       console.error(error);
+  //     });
+  // };
 
   return (
     <div>
       <input type="file" onChange={handleFileChange} />
-      <button onClick={handleFileUpload}>Upload</button>
+      <button onClick={handleUpload}>Upload</button>
     </div>
   );
 }
