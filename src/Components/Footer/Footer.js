e0ae9405f8549bfd7e04482af9cb11bbf6d12c73
Goose <victoriaplatt@loveplanes.co.uk>
2023-06-28 18:30:51 +0100

    Footer component working, needs further tweaks

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Footer/Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer-inner">
+          <h1>I am so over this</h1>
+          <p>Steve, we're not friends anymore :(</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
